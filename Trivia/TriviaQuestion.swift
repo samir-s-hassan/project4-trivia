3f13da6a3ca6c0def69732ca537de58f2e080ce4
samir-s-hassan <hsamir347@gmail.com>
2024-03-18 17:42:48 -0400

    making the struct for TriviaQuestion into Decodable

--- Trivia/TriviaQuestion.swift
+++ Trivia/TriviaQuestion.swift
@@ -7,9 +7,26 @@
 
 import Foundation
 
-struct TriviaQuestion {
+struct TriviaQuestionResponse: Decodable {
+    let results: [TriviaQuestion]
+
+    private enum CodingKeys: String, CodingKey {
+        case results = "results" // = "results" not neccesary but did just to show
+    }
+}
+
+struct TriviaQuestion: Decodable {
   let category: String
   let question: String
   let correctAnswer: String
   let incorrectAnswers: [String]
+
+    private enum CodingKeys: String, CodingKey {
+        case category //if the case name exactly matches the JSON key, then we don't need to specify the raw value
+        case question
+        case correctAnswer = "correct_answer"
+        case incorrectAnswers = "incorrect_answers"
+    }
+
 }
+
---
+++ Trivia/TriviaQuestionService.swift
@@ -0,0 +1,10 @@
+//
+//  TriviaQuestionService.swift
+//  Trivia
+//
+//  Created by Samir Hassan on 3/18/24.
+//
+
+import Foundation
+import UIKit
+
